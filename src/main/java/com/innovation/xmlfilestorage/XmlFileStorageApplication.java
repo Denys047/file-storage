785c87a222a6e6ffdcd7e6d6f5bd4352240a9e5d
Denys <dendet94@gmail.com>
2025-03-19 18:24:06 +0300

    init-commit

---
+++ src/main/java/com/innovation/xmlfilestorage/XmlFileStorageApplication.java
@@ -0,0 +1,13 @@
+package com.innovation.xmlfilestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlFileStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmlFileStorageApplication.class, args);
+    }
+
+}
